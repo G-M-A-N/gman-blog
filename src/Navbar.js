14f2b471fb74ec4262e465357d13a9be3eccfdb9
G-M-A-N <gmanngesh@gmail.com>
2022-10-25 00:31:06 +0300

    create navbar with h1 and links

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const navbar = () => {
+    return ( 
+    <nav className="navbar">
+<h1 className="blog-name">Futurirstic Blog</h1>
+<div className="links">
+    <a href="/home">Home</a>
+    <a href="/New">New blog</a>
+    <a href="/Contact"> Contacts</a>
+</div>
+
+
+    </nav>
+     );
+}
+ 
+export default navbar;
